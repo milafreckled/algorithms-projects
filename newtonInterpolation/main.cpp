e7ad12c81d30653aee53a6527f3f949a258bfb83
milafreckled <miladul2014@gmail.com>
2020-11-20 17:17:56 +0100

    Initial Commit

---
+++ newtonInterpolation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  newtonInterpolation
+//
+//  Created by Людмила Маломуж on 20/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
