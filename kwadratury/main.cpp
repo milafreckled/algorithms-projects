ba8be4f0d2ba524c82c9cc4eb7136b909fab759f
milafreckled <miladul2014@gmail.com>
2020-11-23 21:48:27 +0100

    Initial Commit

---
+++ kwadratury/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  kwadratury
+//
+//  Created by Людмила Маломуж on 23/11/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
