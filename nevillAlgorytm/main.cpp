37039b7e543bd1974718431bf9fd3dd32b116d3b
milafreckled <miladul2014@gmail.com>
2020-12-01 08:36:08 +0100

    Initial Commit

---
+++ nevillAlgorytm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  nevillAlgorytm
+//
+//  Created by Людмила Маломуж on 01/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
