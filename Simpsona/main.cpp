67241902d5fca9f9bcc34effb72518fc23818609
milafreckled <miladul2014@gmail.com>
2020-12-04 16:24:44 +0100

    Initial Commit

---
+++ Simpsona/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Simpsona
+//
+//  Created by Людмила Маломуж on 04/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
