7292691349ed1e55093adfc3d454a1689853a68b
milafreckled <miladul2014@gmail.com>
2020-12-06 18:57:12 +0100

    Added Simpson's approximation

--- Simpsona/main.cpp
+++ Simpsona/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
-
+using namespace std;
+float simpson(int a, int b, int w){
+    float h = (b - a)/(w*1.0);
+    float xi = a+h;
+    float res = (1/(2*a))+(1/(2*b));
+    for (int i = 1; i<w; i++){
+        if (i%2!=0){
+            res += 4 * (1/(2*xi));
+        }else{
+            res += 2 *(1/(2*xi));
+        }
+        xi += h;
+    }
+    res *= h/3;
+    return res;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int a = 2;
+    int b  = 4;
+    int w = 4;
+    cout << "Wartosc calki od 2 do 4: "<<simpson(a, b, w)<<endl;
     return 0;
 }
